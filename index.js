d416f48a4f57536ae7b396de5c78d65359913a8c
Kenny Hung (he) <kenny.cfg@outlook.com>
2026-09-08 19:08:57 +0100

    My first POST route

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('You smell');
-})
+});
+
+app.post('/create-user', (req, res) => {
+  res.send('Got a POST request');
+});
 
 app.listen(port, () => {
   console.log(`Server really is now running on port ${port}`);
