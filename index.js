e63b0510c38393f52b0186d664f10e35f57ba049
Kenny Hung (he) <kenny.cfg@outlook.com>
2026-09-08 19:02:30 +0100

    dev script with nodemon

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const app = express();
 const port = 3000;
 
 app.listen(port, () => {
-  console.log(`Server now running on port ${port}`);
+  console.log(`Server really is now running on port ${port}`);
 });
